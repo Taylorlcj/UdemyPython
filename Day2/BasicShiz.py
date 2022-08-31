d138cc30876f1c953fba0cb89e0d74b3a597b952
Taylor Jensen <Taylorlcj@gmail.com>
2022-08-31 00:00:29 -0400

    day 2

--- Day2/BasicShiz.py
+++ Day2/BasicShiz.py
@@ -1,6 +1,6 @@
 # prints and gets the data type of the variable
-print(type(num_char))
 num_char = len(input("What is your name?"))
+print(type(num_char))
 new_num_char = str(num_char)
 print("Your name has " + new_num_char + " characters")
 
